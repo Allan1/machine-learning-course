c716e16e450c2ec82a12e808e48c338a7d8dd1ff
Allan <allanoliveira.main@gmail.com>
2018-07-31 23:03:49 -0300

    First commit; section 3, class 14

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -0,0 +1,17 @@
+# Data Preprocessing
+
+#Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+#Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy='mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3]) #Left limit included, right limit not included
+X[:, 1:3] = imputer.transform(X[:, 1:3])
